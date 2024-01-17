f29d7a010f60aa0774c73c776abc11338a38612a
Alena Sobolieva <soboleva13as@gmail.com>
2024-01-17 17:28:53 +0200

    tags_add_delete_update

--- src/repositories/tags.py
+++ src/repositories/tags.py
@@ -1,80 +1,47 @@
-from sqlalchemy import select, update, delete, insert
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from src.database.models import Tag, Publication, PublicationTagAssociation
-
 from src.schemas.tags import TagBase, TagUpdate
-from typing import Sequence
-
 from src.utils.my_logger import logger
 
 
 async def create_tags(body_tags: list[TagBase], db: AsyncSession):
     tags = []
     for body in body_tags:
-        # Check if the tag already exists
         stmt = select(Tag).where(Tag.name == body.name)
         result = await db.execute(stmt)
         tag = result.scalar_one_or_none()
 
         if tag is None:
-            # If the tag doesn't exist, create it
             tag = Tag(name=body.name)
             db.add(tag)
             await db.commit()
             await db.refresh(tag)
         tags.append(tag)
-        logger.info(f'tag created: {tag}')
+        logger.info(f'Tag created: {tag.name}')
     return tags
 
 
 async def append_tags_to_publication(publication, tags: list[Tag]):
+    # Limit to add maximum 5 tags
+    if len(publication.tags) + len(tags) > 5:
+        raise ValueError("Exceeded the maximum allowed tags for a publication (5).")
+
     for tag in tags:
         publication.tags.append(tag)
     return publication
 
 
-
-
-
-
-
-
-
-
-
-# async def delete_tag(tag_id: int, db: AsyncSession) -> None:
-#     """
-#     Delete a tag by ID.
-#     """
-#     stmt = delete(Tag).where(Tag.id == tag_id)
-#     await db.execute(stmt)
-#     await db.commit()
-#
-#
-# async def get_tag(tag_id: int, db: AsyncSession) -> Tag | None:
-#     """
-#     Get a tag by ID.
-#     """
-#     stmt = select(Tag).filter_by(id=tag_id)
-#     tag = await db.execute(stmt)
-#     return tag.scalar_one_or_none()
-#
-#
-# async def get_tags(db: AsyncSession) -> Sequence[Tag]:
-#     """
-#     Get all tags.
-#     """
-#     stmt = select(Tag)
-#     tags = await db.execute(stmt)
-#     return tags.scalars().all()
-#
-#
-# async def edit_tag(tag_id: int, tag_update: TagUpdate, db: AsyncSession) -> Tag | None:
-#     """
-#     Edit a tag by ID.
-#     """
-#     stmt = update(Tag).where(Tag.id == tag_id).values(tag_update.dict(exclude_unset=True))
-#     await db.execute(stmt)
-#     await db.commit()
-#     return await get_tag(tag_id, db)
+async def delete_tags_from_publication(publication, tags: list[Tag], db: AsyncSession):
+    for tag in tags:
+        if tag in publication.tags:
+            publication.tags.remove(tag)
+            # TODO: Check if the tag is not associated with any other publications, delete if not
+            stmt = select(PublicationTagAssociation).filter_by(tag_id=tag.id)
+            result = await db.execute(stmt)
+            association_count = result.scalar()
+            if association_count == 0:
+                await db.delete(tag)
+                logger.info(f'Tag deleted: {tag.name}')
+    await db.commit()
+    return publication
--- src/routing/tags.py
+++ src/routing/tags.py
@@ -1,64 +1,57 @@
-from typing import List
-
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
-
-from src.schemas.tags import TagCreate, TagUpdate, TagPublication, TagBase
-from src.schemas.publications import PublicationResponse
-from src.repositories import tags as repository_tags
+from starlette import status
 from src.database.db import get_db
+from src.database.models import User, Tag
+from src.repositories import tags as repositories_tags
+from src.repositories import publications as repositories_publications
 from src.services.auth import auth_service
-from src.database.models import User, Publication, PublicationTagAssociation
+from src.schemas.tags import TagBase, TagPublication, TagCreate
 import src.messages as msg
 
-router = APIRouter(prefix='/tags', tags=['tags'])
+router = APIRouter(prefix='/tags', tags=['Tags'])
 
 
-@router.put('/add_to_publication', response_model=PublicationResponse, status_code=201,
-            description='Add a new tag to pub by id')
-async def add_tag_to_publication(publication_id: int, body: TagBase, db: AsyncSession = Depends(get_db),
-                                 user: User = Depends(auth_service.get_current_user)):
-    stmt = select(Publication).filter_by(id=publication_id, user=user)
-    publication = await db.execute(stmt)
-    publication = publication.scalar_one_or_none()
+@router.post('/create', status_code=status.HTTP_201_CREATED, response_model=list[TagPublication])
+async def create_tags(tags: list[TagCreate], db: AsyncSession = Depends(get_db),
+                      user: User = Depends(auth_service.get_current_user)):
+    created_tags = await repositories_tags.create_tags(tags, db)
+    return [{"id": tag.id, "name": tag.name} for tag in created_tags]
+
+
+@router.post('/add_to_publication', status_code=status.HTTP_200_OK, response_model=list[TagPublication])
+async def add_tags_to_publication(publication_id: int, tags: list[TagBase], db: AsyncSession = Depends(get_db),
+                                  user: User = Depends(auth_service.get_current_user)):
+    publication = await repositories_publications.get_publication(publication_id, db, user)
+
     if publication is None:
         raise HTTPException(status_code=404, detail=msg.PUBLICATION_NOT_FOUND)
 
-    tags = await repository_tags.create_tags([body], db)
-    insert_stmt = insert(PublicationTagAssociation).values(publication_id=publication_id, tag_id=tags[0].id)
-    await db.commit()
-    await db.refresh(publication)
-    return publication
+    existing_tags = set(tag.name for tag in publication.tags)
+    new_tags = [tag for tag in tags if tag.name not in existing_tags]
 
-# @router.post('/add', response_model=TagPublication, status_code=201, description='Add a new tag')
-# async def add_tag(tag: TagCreate, db: AsyncSession = Depends(get_db), user: User = Depends(auth_service.get_current_user)):
-#     created_tag = await repository_tags.add_tag(tag, db)
-#     return created_tag
-#
-# @router.delete('/{tag_id}/delete', response_model=None, status_code=204, description='Delete a tag by ID')
-# async def delete_tag(tag_id: int, db: AsyncSession = Depends(get_db), user: User = Depends(auth_service.get_current_user)):
-#     await repository_tags.delete_tag(tag_id, db)
-#     return None
-#
-# @router.get('/{tag_id}', response_model=TagPublication, description='Get a tag by ID')
-# async def get_tag(tag_id: int, db: AsyncSession = Depends(get_db), user: User = Depends(auth_service.get_current_user)):
-#     tag = await repository_tags.get_tag(tag_id, db)
-#     if tag is not None:
-#         return tag
-#     else:
-#         raise HTTPException(status_code=404, detail=msg.TAG_NOT_FOUND)
-#
-# @router.get('/', response_model=List[TagPublication], description='Get all tags')
-# async def get_tags(db: AsyncSession = Depends(get_db), user: User = Depends(auth_service.get_current_user)):
-#     tags = await repository_tags.get_tags(db)
-#     return tags
-#
-# @router.patch('/{tag_id}/edit', response_model=TagPublication, description='Edit a tag by ID')
-# async def edit_tag(tag_id: int, tag_update: TagUpdate, db: AsyncSession = Depends(get_db),
-#                    user: User = Depends(auth_service.get_current_user)):
-#     edited_tag = await repository_tags.edit_tag(tag_id, tag_update, db)
-#     if edited_tag is not None:
-#         return edited_tag
-#     else:
-#         raise HTTPException(status_code=404, detail=msg.TAG_NOT_FOUND)
+    if len(publication.tags) + len(new_tags) > 5:
+        raise HTTPException(status_code=400, detail="Exceeded the maximum allowed tags for a publication (5).")
+
+    added_tags = await repositories_tags.create_tags(new_tags, db)
+    publication = await repositories_tags.append_tags_to_publication(publication, added_tags)
+
+    return [{"id": tag.id, "name": tag.name} for tag in publication.tags]
+
+
+@router.delete('/remove_from_publication', status_code=status.HTTP_200_OK, response_model=list[TagPublication])
+async def remove_tags_from_publication(publication_id: int, tags: list[TagBase], db: AsyncSession = Depends(get_db),
+                                       user: User = Depends(auth_service.get_current_user), repositories=None):
+    publication = await repositories_publications.get_publication(publication_id, db, user)
+
+    if publication is None:
+        raise HTTPException(status_code=404, detail=msg.PUBLICATION_NOT_FOUND)
+
+    tags_to_remove = [tag for tag in publication.tags if tag.name in [t.name for t in tags]]
+
+    if not tags_to_remove:
+        raise HTTPException(status_code=400, detail="No matching tags found to remove from the publication.")
+
+    publication = await repositories_tags.delete_tags_from_publication(publication, tags_to_remove, db)
+
+    return [{"id": tag.id, "name": tag.name} for tag in publication.tags]
