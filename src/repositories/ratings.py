f13a443cbb6b9821a928e73864ebf5285103ed72
SergiyBagmet <134723713+SergiyBagmet@users.noreply.github.com>
2024-01-22 13:00:12 +0200

    view users who add rating to publication by pub_id

--- src/repositories/ratings.py
+++ src/repositories/ratings.py
@@ -13,7 +13,14 @@ async def add_rating(publication_id: int, body: RatingCreate, db: AsyncSession, 
     return rating
 
 
-async def get_all_ratings_by_user_id(user_id: int, db: AsyncSession):
-    stmt = select(Rating).filter_by(user_id=user_id)
+async def get_all_ratings_by_user_id(user_id: int, db: AsyncSession, limit: int, offset: int):
+    stmt = select(Rating).filter_by(user_id=user_id).limit(limit).offset(offset)
     ratings = await db.execute(stmt)
     return ratings.unique().scalars().all()
+
+
+async def get_users_by_ratings(ratings: list[Rating], db: AsyncSession, limit: int, offset: int):
+    stmt = (select(User).filter(User.id.in_([rating.user_id for rating in ratings]))
+            .limit(limit).offset(offset))
+    users = await db.execute(stmt)
+    return users.scalars().all()
--- src/routing/ratings.py
+++ src/routing/ratings.py
@@ -8,6 +8,7 @@ from src.database.models import User, Publication, Rating, Role
 from src.repositories import publications as repositories_publications
 from src.repositories import ratings as repositories_ratings
 from src.schemas.ratings import RatingCreate, RatingResponse
+from src.schemas.user import UserResponse
 from src.services.auth import auth_service
 from src.services.roles import RoleAccess
 from src.utils.my_logger import logger
@@ -17,6 +18,7 @@ router = APIRouter(tags=['rating'])
 access_to_route = RoleAccess([Role.admin, Role.moderator])
 
 
+# all roles
 @router.post('/publications/{publication_id}/rating', status_code=status.HTTP_201_CREATED,
              response_model=RatingResponse)
 async def add_rating(publication_id: int, body: RatingCreate, db: AsyncSession = Depends(get_db),
@@ -36,10 +38,26 @@ async def add_rating(publication_id: int, body: RatingCreate, db: AsyncSession =
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=msg.ALREADY_VOTED_PUBLICATION)
 
 
+# all roles
+@router.get('/publications/{publication_id}/rating', status_code=status.HTTP_200_OK, response_model=list[UserResponse])
+async def get_users_ratings_by_publication_id(publication_id: int, db: AsyncSession = Depends(get_db),
+                                              limit: int = Query(10, ge=10, le=500), offset: int = Query(0, ge=0),
+                                              user: User = Depends(auth_service.get_current_user)):
+
+    publication = await repositories_publications.get_publication_by_id(publication_id, db, user)
+    if publication is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=msg.PUBLICATION_NOT_FOUND)
+
+    users = await repositories_ratings.get_users_by_ratings(publication.ratings, db, limit, offset)
+    return users
+
+
+# admin, moderator only
 @router.get('/{user_id}/rating', status_code=status.HTTP_200_OK, response_model=list[RatingResponse],
             dependencies=[Depends(access_to_route)])
 async def get_user_ratings(user_id: int, db: AsyncSession = Depends(get_db),
+                           limit: int = Query(10, ge=10, le=500), offset: int = Query(0, ge=0),
                            user: User = Depends(auth_service.get_current_user)):
 
-    ratings = await repositories_ratings.get_all_ratings_by_user_id(user_id, db)
+    ratings = await repositories_ratings.get_all_ratings_by_user_id(user_id, db, limit, offset)
     return ratings
